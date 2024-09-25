b39349fae0b1384b49a0d878026af583e0603ebb
Ricardo Cezar <ricardorcr2@gmail.com>
2024-09-24 22:25:25 -0300

    Criando despesas sem erro

--- src/Despesa/index.jsx
+++ src/Despesa/index.jsx
@@ -1,6 +1,34 @@
+import { useEffect, useState } from 'react'
 import './style.css'
+import axios from 'axios';
 
 export default function Despesa() {
 
-    return <>Lista de Despesas</>
+    const [despesas, setDespesas] = useState([])
+
+    async function getDespesas() {
+        const response = await axios.get('http://localhost:8080/despesa');
+        setDespesas(response.data)
+    }
+
+    async function getReceitas() {
+        const response = await axios.get('http://localhost:8080/receita');
+        setDespesas(response.data)
+    }
+
+    useEffect(() => {
+        getDespesas()
+    }, [])
+
+    return (
+        <div>
+            <p>Lista de Despesas</p>
+            {despesas.map(despesa => (
+                <>
+                {despesa.descricao} / {despesa.categoria} / {despesa.valor} 
+                <br />
+                </>
+            ))}
+        </div>
+    );
 }
